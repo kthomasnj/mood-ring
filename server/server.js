33a4109e5674efc219d2becc36643af9ef5bf540
kthomasnj <kthomasnj@gmail.com>
2022-11-28 21:09:40 -0500

    Updated server.js file

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
-const { typeDefs, resolvers } = require('./schema');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
