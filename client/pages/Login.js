608918b24304f37e2edb4cd48e23404f01de1ace
Vanese <vanesebuchanan@gmail.com>
2022-11-29 16:26:11 -0500

    login page2

--- client/pages/Login.js
+++ client/pages/Login.js
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
 
 export const Login = () => {
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+    }
+
     return (
-        <form>
+        <form onLogin={handleLogin}>
             <label for='username'>Username</label>
-            <input type='username' placeholder='username' id='username' name='username' />
+            <input value={userName} onChange={(e) => setUserName(e.target.value)}type='username' placeholder='username' id='username' name='username' />
             <label for='password'>Password</label>
-            <input type='password' placeholder='' id='password' name='password' />
+            <input value={password} onChange={(e) => setPassword(e.target.value)}type='password' placeholder='' id='password' name='password' />
             <button>Login</button>
         </form>
     )
 
-    // const [userName, setUserName] = useState('');
-    // const [password, setPassword] = useState('');
+    
 }
