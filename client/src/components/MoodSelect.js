4c1b0eabfa322ab822c0789d73109e8a77391a7c
bilalfarrukh2000 <bilalfarrukh2000@gmail.com>
2022-12-02 18:03:23 -0500

    Add Transition to Depression

--- client/src/components/MoodSelect.js
+++ client/src/components/MoodSelect.js
@@ -9,12 +9,20 @@ function MoodSelect (){
       const selectedMood={mood};
       console.log(selectedMood.mood);
       if(selectedMood.mood=='sad'){
-        console.log("mood is sad");
-        document.body.style.backgroundColor = "maroon";
+        
+        
+        document.body.style.backgroundColor = "#1515b6";
       }
       else if(selectedMood.mood=='happy'){
         document.body.style.cssText = 
         `background: green;
+         transition: all 1s ease;
+         WebkitTransition: all 1s ease;
+         MozTransition: all 1s ease;`;
+      }
+      else{
+        document.body.style.cssText = 
+        `background: maroon;
          transition: all 1s ease;
          WebkitTransition: all 1s ease;
          MozTransition: all 1s ease;`;
