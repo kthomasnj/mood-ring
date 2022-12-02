de84b7b23d1fe65e07f8a8499880342fa6954fcd
hasnafar <hasnafarrukh@gmail.com>
2022-12-01 19:22:09 -0500

    set up homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,68 @@
-import React, { useState } from 'react';
-import './App.css';
-// import { Login } from './pages/Login';
-import { Register } from './pages/Register';
-import Music from './pages/Music';
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Header from './components/Header';
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-    return (
-    <div className="App">
-       <Register />  
-       <Music url={['https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3']} />
-    </div>
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              {/* <Route 
+                path="/"
+                element={<Home />}
+              /> */}
+              <Route 
+                path="/login" 
+                element={<Login />}
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />}
+              />
+            </Routes>
+          </div>
+          {/* <Footer /> */}
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
