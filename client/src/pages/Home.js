cc27a9a1711752818455cfcd63736b9a4f58cf63
kthomasnj <kthomasnj@gmail.com>
2022-12-06 09:30:18 -0500

    Uploading sounds

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import MoodSelect from '../components/MoodSelect';
+import useSound from 'use-sound';
 
 import Auth from '../utils/auth';
 
