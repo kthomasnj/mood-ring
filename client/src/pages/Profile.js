74269cb9088b01f34effa095f2964c6ea7cdbd3f
devin <devin@devins-MacBook-Pro.local>
2022-12-06 15:49:39 -0500

    edits to profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -53,22 +53,18 @@ const Profile = () => {
         </div>
       </header>
     <div>
-      <div className="flex-row justify-center mb-3">
-        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+      <div className="flex-row justify-center mb-3 my-profile-1">
+        <h2 className="col-12 col-md-10 text-center my-profile">
+          {userParam ? `${user.username}'s` : `${user.username}'s`} profile
         </h2>
-
-        <div className="col-12 col-md-10 mb-5">
-         
-        </div>
-        {!userParam && (
-          <div
-            className="col-12 col-md-10 mb-3 p-3"
-            style={{ border: '1px dotted #1a1a1a' }}
-          >
-          </div>
-        )}
       </div>
+    </div>
+    <div className='container'>
+      <h3 className='text-center col-12 my-favorites'>Favorites</h3>
+      <p className='col-12 saved-moods'>Saved Mood - 1</p>
+      <p className='col-12 saved-moods'>Saved Mood - 2</p>
+      <p className='col-12 saved-moods'>Saved Mood - 3</p>
+      <p className='col-12 saved-moods'>Saved Mood - 4</p>
     </div>
     </body>
   );
